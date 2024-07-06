195a77f65d87144e7f8d02cca20b18b44f5dc154
JustGithubProject <Kropivadanil@gmail.com>
2024-07-06 08:34:15 +0300

    Created database and added user to this database (Fixed get_all_users endpoint)

--- src/models/pydantic_models.py
+++ src/models/pydantic_models.py
@@ -1,13 +1,20 @@
-from pydantic import BaseModel
+from bson import ObjectId
+from pydantic import (
+    BaseModel,
+    Field
+)
+
 
 
 class UserModel(BaseModel):
+    id: str
     username: str
     email: str
     password: str
-
+    
 
 class VideoModel(BaseModel):
+    id: str
     title: str
     user_id: str
     video_path: str
--- src/models/users.py
+++ src/models/users.py
@@ -2,16 +2,19 @@ class User:
     """Model to interact with user_collection"""
     def __init__(
         self,
+        id: str,
         username: str,
         email: str,
         password: str
     ) -> None:
+        self.id = id
         self.username = username
         self.email = email
         self.password = password
     
     def to_dict(self) -> dict:
         return {
+            "id": self.id,
             "username": self.username,
             "email": self.email,
             "password": self.password
