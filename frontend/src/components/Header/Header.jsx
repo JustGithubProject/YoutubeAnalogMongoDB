7a1539779858967eed15ca077693fda902127185
JustGithubProject <Kropivadanil@gmail.com>
2024-07-07 11:09:58 +0300

    Added a condition for displaying your items to authorized and unauthorized users

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,23 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css';
-import logo from "./logo.png";
+import logo from "./images/new_logo.png";
+import notification from "./images/notification.png"
+import addVideo from "./images/ipad.png"
+import profile from "./images/profile.png"
+import { ReactComponent as SearchIcon } from './glass.svg';
 
 const Header = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    useEffect(() => {
+        // Проверяем наличие токена в localStorage
+        const accessToken = localStorage.getItem('accessToken');
+        if (accessToken) {
+            setIsAuthenticated(true);
+        } else {
+            setIsAuthenticated(false);
+        }
+    }, []);
+
     return (
         <header className="header">
             <div className="logo">
-                <img src={logo} alt="Логотип" />
+                <a href="/">
+                    <img src={logo} alt="Логотип" />
+                </a>
             </div>
             <div className="search">
-                <input type="text" placeholder="Поиск..." />
-                <button>Найти</button>
+                <input type="text" placeholder="Search..." style={{ width: '400px' }} /> 
+                <button><SearchIcon className="search-icon" /></button> 
             </div>
             <nav className="nav">
                 <ul>
-                    <li><a href="/">Главная</a></li>
-                    <li><a href="/about">О нас</a></li>
-                    <li><a href="/services">Услуги</a></li>
-                    <li><a href="/contact">Контакты</a></li>
+                    {!isAuthenticated && (
+                        <>
+                            <li><a href="/register">Sign up</a></li>
+                            <li><a href="/login">Sign in</a></li>
+                        </>
+                    )}
+                   {isAuthenticated && (
+                    <>
+                        <li><img src={notification} alt="notification"/></li>
+                        <li><img src={addVideo} alt="addVideo"/></li>
+                        <li><img src={profile} alt="Profile" /></li>
+                    </>
+                   )}
+                    
                 </ul>
             </nav>
         </header>
