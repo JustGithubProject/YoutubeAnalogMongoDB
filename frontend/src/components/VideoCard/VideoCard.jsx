b220380f4b11a12501a20e92c08b5fd5f5b1c4eb
JustGithubProject <Kropivadanil@gmail.com>
2024-07-07 14:20:50 +0300

    Created custom image for frontend and nginx for maintenance

--- frontend/src/components/VideoCard/VideoCard.jsx
+++ frontend/src/components/VideoCard/VideoCard.jsx
@@ -4,11 +4,6 @@ import './VideoCard.css';
 const VideoCard = () => {
   return (
     <div className="video-card">
-      {/* <img
-        src="https://placeimg.com/300/100/any"
-        alt="Sample Video"
-        className="thumbnail"
-      /> */}
       <div className="video-info">
         <h3 className="video-title">Sample Video Title</h3>
         <p className="channel">Channel Name</p>
