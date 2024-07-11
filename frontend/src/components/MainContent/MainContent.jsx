9ebd4b231780787c71fe8b1b2079d2eb4bee255c
JustGithubProject <Kropivadanil@gmail.com>
2024-07-11 09:20:55 +0300

    Fixed problem with images

--- frontend/src/components/MainContent/MainContent.jsx
+++ frontend/src/components/MainContent/MainContent.jsx
@@ -24,7 +24,7 @@ const MainContent = () => {
     };
 
     fetchVideos();
-  }, [page]);
+  }, [page, videos]);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -46,7 +46,7 @@ const MainContent = () => {
         observer.unobserve(currentLoaderRef);
       }
     };
-  }, [hasMore]);
+  }, [hasMore, videos]);
 
   const chunkArray = (array, chunkSize) => {
     const chunks = [];
--- frontend/src/components/VideoCard/VideoCard.jsx
+++ frontend/src/components/VideoCard/VideoCard.jsx
@@ -3,12 +3,13 @@ import './VideoCard.css';
 
 const VideoCard = ({video}) => {
   console.log(video.preview_image_path)
+  const pathToImage = `http://127.0.0.1:8080${video.preview_image_path}`;
   return (
     <div className="video-card">
       <div className="video-info">
         <h3 className="video-title">{video.title}</h3>
         <p className="description">{video.description}</p>
-        <img src={video.preview_image_path} alt="Preview" className="preview-image" />
+        <img src={pathToImage} alt="Preview" className="preview-image" />
       </div>
     </div>
   );
